2801353b914e4e79ade20d64bc8b734aa493bc5c
rustfuture <yalikoyrecordz@gmail.com>
2025-07-01 14:06:41 +0000

    feat: Untrack and remove all build artifacts from Git history
    The entire  directory, containing all build artifacts, has been untracked and removed from the Git history. These files are automatically generated during compilation and should not be part of the version control. This action ensures that GitHub's language statistics accurately reflect the project's primary language (Rust) by excluding generated files that were incorrectly identified as Makefiles or other languages.

--- target/debug/build/crossbeam-utils-88b8605be26ef90c/build_script_build-88b8605be26ef90c.d
+++
@@ -1,9 +0,0 @@
-/home/macintosh/Desktop/rustprojects/RustHound/target/debug/build/crossbeam-utils-88b8605be26ef90c/build_script_build-88b8605be26ef90c.d: /home/macintosh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-utils-0.8.21/build.rs /home/macintosh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-utils-0.8.21/no_atomic.rs /home/macintosh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-utils-0.8.21/build-common.rs
-
-/home/macintosh/Desktop/rustprojects/RustHound/target/debug/build/crossbeam-utils-88b8605be26ef90c/build_script_build-88b8605be26ef90c: /home/macintosh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-utils-0.8.21/build.rs /home/macintosh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-utils-0.8.21/no_atomic.rs /home/macintosh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-utils-0.8.21/build-common.rs
-
-/home/macintosh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-utils-0.8.21/build.rs:
-/home/macintosh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-utils-0.8.21/no_atomic.rs:
-/home/macintosh/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-utils-0.8.21/build-common.rs:
-
-# env-dep:CARGO_PKG_NAME=crossbeam-utils
